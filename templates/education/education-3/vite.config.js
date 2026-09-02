030da8788005c02444ce4631380e9a70d572a906
vishalav2005 <vishalav604@gmail.com>
2026-09-02 19:13:02 +0530

    feat: add admin-10, education-2, education-3, and agency-8 template folders and public previews

---
+++ templates/education/education-3/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.js.org/config/
+export default defineConfig({
+  base: "/templates/education/education-3/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
