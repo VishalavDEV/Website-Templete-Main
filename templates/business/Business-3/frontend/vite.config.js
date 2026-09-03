3dd2b38421d49dc985b8fe7c3005cd1c9654e162
vishalav2005 <vishalav604@gmail.com>
2026-09-03 08:34:49 +0530

    fix(business): flatten nested frontend dirs, remove hashrouter basenames, inject preview topbars and rebuild all 10 Business templates

--- templates/business/Business-3/frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/templates/buisness/Business-3/',
-  plugins: [react()],
-})
--- templates/business/Business-6/src/App.jsx
+++ templates/business/Business-6/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import NexoraPage from './pages/NexoraPage';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/templates/buisness/Business-6">
+    <HashRouter >
       <NexoraPage />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
